d9c693fced1c2baf17eb234fb9a7b6563ea072f5
Dadilll <Danil2ka2142@yandex.ru>
2024-02-25 01:28:40 +0700

    доработка

--- cmd/agentmain/main.go
+++ cmd/agentmain/main.go
@@ -4,22 +4,18 @@ import (
 	"context"
 	"log"
 
+	"github.com/Dadil/project/config"
 	"github.com/Dadil/project/internal/agent/agent"
-	"github.com/go-redis/redis/v8"
 )
 
 func main() {
-	redisClient := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
+
+	redisClient := config.NewRedisClient()
 
 	if _, err := redisClient.Ping(context.Background()).Result(); err != nil {
 		log.Fatalf("Could not connect to Redis: %v", err)
 	}
 
-	// Если мы дошли до этой точки, подключение к Redis прошло успешно
 	log.Println("Connected to Redis")
 	// Создание и запуск агентов
 	numAgents := 3
